647751714f50821edbf073d427b615fbd1ba80c9
Heshie Brody <4hbrody@gmail.com>
2021-04-09 16:25:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,8 +5,9 @@ def badge_maker(name)
 end
 #return array with personalized badge messages
 def batch_badge_creator(names)
+  msg_lst = []
   names.each do |name|
-    badge_maker(name)
+    msg_list << badge_maker(name)
   end
 end
 #return assigned room messages in an array
