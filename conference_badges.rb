557aad28029f0bc5e16860766cd85bbe8d881b01
Heshie Brody <4hbrody@gmail.com>
2021-04-09 16:10:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,8 @@
 # Write your code here.
 #returns badge message with name
-
+def badge_maker(name)
+  puts"Hello, my name is #{name}."
+end
 #return array with personalized badge messages
 
 #return assigned room messages in an array
