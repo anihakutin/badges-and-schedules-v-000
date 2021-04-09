a6c5bd1237efd1c2ae5718eec8783137c431cf03
Heshie Brody <4hbrody@gmail.com>
2021-04-09 16:14:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,11 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 #return array with personalized badge messages
-
+def batch_badge_creator(names)
+  names.each do |name|
+    badge_maker(name)
+  end
+end
 #return assigned room messages in an array
 
 #printer
